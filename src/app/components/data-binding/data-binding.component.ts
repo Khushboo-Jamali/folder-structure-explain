0cf972fdc24627d21f730755db3ea36b76304d48
khushboo-jamali <jamalikhushboo@gmail.com>
2025-09-01 18:26:24 +0500

    angular

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,24 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.css'
+  styleUrl: './data-binding.component.css',
 })
 export class DataBindingComponent {
-  courseName : string = "Angular full course";
-  courseDuration : number =22344465456;
+  courseName: string = 'Angular full course';
+  courseDuration: number = 22344465456;
   nameMinLenght: number = 5;
-  myInputType: string= "date";
-  myClass: string= "text-danger";
+  myInputType: string = 'date';
+  myClass: string = 'text-danger';
 
-  showMsg(){
-    alert('Hello welcome to our angular practice section')
+  showMsg() {
+    alert('Hello welcome to our angular practice section');
   }
-  OnCity(event:Event){
-    debugger
-    alert('congratulations city changed successfully')
+  OnCity(event: any) {
+    debugger;
+    const name = event.target?.value;
+    alert('congratulations city changed successfully city name is > ' + name);
+  }
+  OnMouseEnter() {
+    console.log('Mouse entered ');
+  }
+  OnMouseLeft() {
+    console.log('Mouse Left div ');
+  }
+  showLanguageName(name: string) {
+    alert(name);
   }
 }
